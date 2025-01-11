7f314dedeace6ae41e4fa018db3f5cc74f32cac7
jian.zhou <jianzhou0420@outlook.com>
2025-01-12 00:01:39 +1030

    1

--- zero/v2/trainer_lotus.py
+++ zero/v2/trainer_lotus.py
@@ -54,8 +54,8 @@ class TrainerLotus(pl.LightningModule):
 
         losses = self.model(batch, is_train=True)
         self.log('train_loss', losses['total'])
-        # if self.global_step % 10 == 0:
-        #     print(f"train_loss: {losses['total']}")
+        if self.global_step % 10 == 0:
+            print(f"train_loss: {losses['total']}")
         # print(f"After loading: {torch.cuda.memory_allocated()} bytes")
         return losses['total']
 
@@ -101,7 +101,7 @@ class TrainerLotus(pl.LightningModule):
 if __name__ == '__main__':
     def train(voxel_size):
         config = yacs.config.CfgNode(new_allowed=True)
-        config.merge_from_file(f'/workspace/zero/zero/v1/config/lotus.yaml')
+        config.merge_from_file(f'/workspace/zero/zero/v1/config/lotus{voxel_size}.yaml')
 
         # config.TRAIN_DATASET.tasks_to_use = ['close_jar']
         trainer_model = TrainerLotus(config)
