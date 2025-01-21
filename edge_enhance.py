a9a3818fb8c0c83af52d9317e7b52515df697351
jian.zhou <jianzhou0420@outlook.com>
2025-01-21 13:07:46 +1030

    move to v3 and clean

--- edge_enhance.py
+++
@@ -1,3 +0,0 @@
-
-picture_file = '/media/jian/ssd4t/selfgen/20250105/train_dataset/keysteps/seed42/close_jar/variation0/episodes/episode0/data.pkl'
-pc_file = ''
--- pg.py
+++
@@ -1,7 +0,0 @@
-
-from zero.v2.models.lotus.utils.rotation_transform import (
-    RotationMatrixTransform, quaternion_to_discrete_euler
-)
-rot = [0, 36, 6]
-gt_rot = quaternion_to_discrete_euler(rot, 5)
-print(gt_rot)
