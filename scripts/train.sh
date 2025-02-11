6f7ab4e49118f7fad34ade9cc9e562aeff7428e1
jian.zhou <jianzhou0420@outlook.com>
2025-02-11 14:52:36 +1030

    adjust gpu and batch size

--- scripts/train.sh
+++ scripts/train.sh
@@ -4,6 +4,6 @@ conda activate zero
 name=expBins_all_together
 config_path=/data/zero/zero/expAugmentation/config/expBase_all_together.yaml
 
-python /data/zero/zero/expAugmentation/trainer_expbase.py --config $config_path --name $name --num_gpu 4
+python /data/zero/zero/expAugmentation/trainer_expbase.py --config $config_path --name $name --num_gpu 2
 
 
