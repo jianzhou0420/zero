0cd9bd72c453aea4f3115d3834024e02cc3038f1
jian.zhou <jianzhou0420@outlook.com>
2025-02-13 10:06:30 +1030

    all together

--- scripts/train.sh
+++ scripts/train.sh
@@ -4,6 +4,6 @@ conda activate zero
 name=expBase_4gpu_1_batch_size
 config_path=/data/zero/zero/expAugmentation/config/expBase_all_together.yaml
 
-python /data/zero/zero/expAugmentation/trainer_expbase.py --config $config_path --name $name --num_gpu 4 --epoches 800 --batch_size 1
+python /data/zero/zero/expAugmentation/trainer_expbase.py --config $config_path --name $name --num_gpu 4 --epoches 1500 --batch_size 1
 
 
