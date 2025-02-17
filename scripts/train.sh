f8cc69056bfa4a16441c502930ac758819f7f41f
jianzhou0420 <jianzhou0420@outlook.com>
2025-02-17 15:11:10 +1030

    111

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,18 +1,18 @@
-
+conda activate zero
 
 python  -m zero.expBaseV5.trainer_expbase \
         --exp-config /data/zero/zero/expBaseV5/config/expBase_Lotus.yaml \
         name expBaseV5_test \
         dataset augment\
         num_gpus 1 \
-        epoches 800 \
+        epoches 1200 \
         batch_size 4 \
         TRAIN_DATASET.num_points 100000 \
         TRAIN_DATASET.pos_bins 75 \
         TRAIN_DATASET.pos_bin_size 0.001\
         MODEL.action_config.pos_bins 75\
         MODEL.action_config.pos_bin_size 0.001 \
-        tasks_to_use "['insert_onto_square_peg','close_jar']" \
+        tasks_to_use "['insert_onto_square_peg','close_jar','light_bulb_in','put_groceries_in_cupboard']" \
         
         
 
