e13bc327bebf054e96ce9c82156f70eee8fe9729
jian.zhou <jianzhou0420@outlook.com>
2025-03-02 13:42:15 +1030

    load roll back

---
+++ scripts/train.sh
@@ -0,0 +1,25 @@
+conda activate zero
+
+tasks_to_use='close_jar'
+
+# tasks_to_use=("meat_off_grill" "sweep_to_dustpan_of_size" "close_jar" "push_buttons" "light_bulb_in" "insert_onto_square_peg" "put_groceries_in_cupboard" "place_shape_in_shape_sorter" "stack_blocks")
+
+python  -m zero.expBaseV5.trainer_expbase \
+        --exp-config /data/zero/zero/expBaseV5_copy/config/expBase_Lotus.yaml \
+        name expBaseV5_test \
+        dataset augment\
+        num_gpus 1 \
+        epoches 400 \
+        batch_size 4 \
+        TRAIN_DATASET.num_points 100000 \
+        TRAIN_DATASET.pos_bins 75 \
+        TRAIN_DATASET.pos_bin_size 0.001\
+        MODEL.action_config.pos_bins 75\
+        MODEL.action_config.pos_bin_size 0.001 \
+        tasks_to_use $tasks_to_use \
+       
+        
+        
+
+
+
